099cbfee7a4cbaac10048b28e8de1de6df46e8c7
moorefantasia <moorefantasia@gmail.com>
2024-09-09 15:30:16 -0500

    added time api

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector(".weather-app-temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description")
-    let humidityElement = document.querySelector("#humidity")
-let windSpeed = document.querySelector("#wind-speed")
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let wind = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
-    console.log(response.data.condition.description);
+    console.log(response.data);
 
     cityElement.innerHTML = response.data.city;
+
+    timeElement.innerHTML = `${date.hetHours()}:${date.getMinutes()}`;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${respone.data.wind.speed}km/h`
+    windElement.innerHTML = `${response.data.wind}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
